0f19370231b7823d2485eb61bc5174c4592602b7
Alison Yao <yy2564@nyu.edu>
2021-05-04 23:43:09 +0800

    present results

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ NODEP-UA 9982.SY1 Experiential Learning Seminar | Final Project
 
 ## Project Description
 
-This is a social science project with a tiny a Data Science (NLP) element. 
+This is a social science project with a tiny Data Science (NLP) element. 
 
 ### Data
 All data is collection from the public domain. They are mostly trancripts from YouTube and also some blog posts and Quora Q&A.
@@ -15,7 +15,11 @@ I mainly conducted qualitative research using interviews and digital ethnography
 ### Quantitative
 
 I also did a little bit of quantitative research by examining the word frequency on all the text corpus. The results support my theories in qualitative research.
--- transcriptConverter.py is used to clean up the format of YouTube transcript after copy pasteing.
--- wordFrequency.py is used to count the frequency of words after some basic data cleaning. Packages used: nltk, matplotlib
 
-*** For more details, please read the report.
+-- transcriptConverter.py is used to clean up the format of YouTube transcript after copy pasteing.
+
+-- wordFrequency.py is used to count the frequency of words after some basic data cleaning. Packages used: nltk, matplotlib.
+
+I will showcase here the code and the majority of data I used for analysis. 
+
+*** For more details, please read the report named quantitative.docx.
--- wordFrequency.py
+++ wordFrequency.py
@@ -7,48 +7,67 @@ nltk.download('wordnet')
 from nltk import word_tokenize, sent_tokenize, FreqDist
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+import matplotlib.pyplot as plt
 # https://becominghuman.ai/nlp-for-beginners-using-nltk-f58ec22005cd
 
-### initialization
-allStopwords = stopwords.words('english')
-lemmatizer = WordNetLemmatizer()
-meaninglessWords = ['.', ',', "'s", "'re", "'d", "'ve", ":", "'m", "n't", "?", "like", '’', ')', '(', 'also', 'data', 'scientist', 'science', 'company', 'thing', 'people', 'think', 'really', 'get', 'one', 'would', 'also', 'work', 'working', 'might', 'job', 'go', '”', '“', 'could', 'wa', 'lot', 'something', 'need', 'want', 'actually', 'startup', 'take', 'say', 'kind', 'even', 'much', 'going', 'ha', 'u', 'know', 'probably', 'may', 'got', 'today', 'bit', 'two', 'yeah', 'yea', 'use', 'course', 'thank']
 
-### read all txt files in the directory
-directory = '/Users/zhuang/Desktop/data'
-txtPaths = []
-for filename in os.listdir(directory):
-    if '.txt' in filename:
-        f = os.path.join(directory, filename)
-        if os.path.isfile(f):
-            txtPaths.append(os.path.join(directory, filename))
-assert len(txtPaths) > 0
-print('#txt:', len(txtPaths))
+def wordFreq(directory, k):
+    ### read all txt files in the directory
+    txtPaths = []
+    for filename in os.listdir(directory):
+        if '.txt' in filename:
+            f = os.path.join(directory, filename)
+            if os.path.isfile(f):
+                txtPaths.append(os.path.join(directory, filename))
+    assert len(txtPaths) > 0
+    print('#txt:', len(txtPaths))
 
-### loop through all txt files
-allTxt = ''
-for txtPath in txtPaths:
-    file1 = open(txtPath, 'r')
-    allText = file1.readlines()
-    if len(allText) > 0:
-        print('ACCESSING', txtPath)
-        transcript = [allText[2].strip('\n')] # ['Data Science: Startup vs. Large Corporation']
-        for line in allText[3:]:
-            line = line[6:].strip('\n')
-            transcript.append(line)
-        transcriptStr = ' '.join(transcript)
-        allTxt += transcriptStr
+    ### loop through all txt files
+    allTxt = ''
+    for txtPath in txtPaths:
+        file1 = open(txtPath, 'r')
+        allText = file1.readlines()
+        if len(allText) > 0:
+            print('ACCESSING:', txtPath)
+            transcript = [allText[2].strip('\n')]
+            for line in allText[3:]:
+                line = line[6:].strip('\n')
+                transcript.append(line)
+            transcriptStr = ' '.join(transcript)
+            allTxt += transcriptStr
 
-### data cleaning & output
-transcriptTokens = word_tokenize(allTxt.lower())
-print('#transcriptTokens:', len(transcriptTokens))
-transcriptTokensLemmatized = [lemmatizer.lemmatize(word) for word in transcriptTokens]
-transcriptTokensLemmatizedCleaned = [word for word in transcriptTokensLemmatized if word not in allStopwords + meaninglessWords]
-print('#transcriptTokensLemmatizedCleaned:', len(transcriptTokensLemmatizedCleaned))
-for i in range(len(transcriptTokensLemmatizedCleaned)):
-    if transcriptTokensLemmatizedCleaned[i] == 'making':
-        transcriptTokensLemmatizedCleaned[i] = 'make'
-    elif transcriptTokensLemmatizedCleaned[i] == 'learning':
-        transcriptTokensLemmatizedCleaned[i] = 'learn'
-freq = FreqDist(transcriptTokensLemmatizedCleaned)
-print(type(freq), freq.most_common(100))
+    ### data cleaning
+    transcriptTokens = word_tokenize(allTxt.lower())
+    print('#transcriptTokens:', len(transcriptTokens))
+    transcriptTokensLemmatized = [lemmatizer.lemmatize(word) for word in transcriptTokens]
+    transcriptTokensLemmatizedCleaned = [word for word in transcriptTokensLemmatized if word not in allStopwords + meaninglessWords]
+    print('#transcriptTokensLemmatizedCleaned:', len(transcriptTokensLemmatizedCleaned))
+    for i in range(len(transcriptTokensLemmatizedCleaned)):
+        if transcriptTokensLemmatizedCleaned[i] == 'making':
+            transcriptTokensLemmatizedCleaned[i] = 'make'
+        elif transcriptTokensLemmatizedCleaned[i] == 'learning':
+            transcriptTokensLemmatizedCleaned[i] = 'learn'
+    # getting frequency & output
+    freq = FreqDist(transcriptTokensLemmatizedCleaned)
+    print(type(freq), freq.most_common(100))
+    return freq.most_common(k)
+
+def visulization(l):
+    words, frequencies = [], []
+    for word_frequency in l:
+        word, frequency = word_frequency
+        words.append(word)
+        frequencies.append(frequency)
+    print(words)
+    print(frequencies)
+    plt.bar(words, frequencies)
+    plt.show() 
+
+
+if __name__ == '__main__':
+    directory = './data'
+    allStopwords = stopwords.words('english')
+    lemmatizer = WordNetLemmatizer()
+    meaninglessWords = ['.', ',', "'s", "'re", "'d", "'ve", ":", "'m", "n't", "?", "like", '’', ')', '(', 'also', 'data', 'scientist', 'science', 'company', 'thing', 'people', 'think', 'really', 'get', 'one', 'would', 'also', 'work', 'working', 'might', 'job', 'go', '”', '“', 'could', 'wa', 'lot', 'something', 'need', 'want', 'actually', 'startup', 'take', 'say', 'kind', 'even', 'much', 'going', 'ha', 'u', 'know', 'probably', 'may', 'got', 'today', 'bit', 'two', 'yeah', 'yea', 'use', 'course', 'thank']
+    mostFreqPairs = wordFreq(directory, 10)
+    visulization(mostFreqPairs)
